48d96d220fc8efcf458260946d47b32a119b4ae7
sherin2014 <2014sherinthomas@gmail.com>
2025-12-11 09:37:47 +0400

    Update API endpoints to use production server

--- client/src/Features/PostSlice.js
+++ client/src/Features/PostSlice.js
@@ -9,7 +9,7 @@ const initialState = {
   
 export const savePost = createAsyncThunk("posts/savePost", async (postData) => {
     try {
-      const response = await axios.post('http://localhost:3001/savePost', {
+      const response = await axios.post(`https://myapp-server-qmdr.onrender.com/savePost`, {
         postMsg: postData.postMsg,
         email: postData.email,
       });
@@ -22,7 +22,7 @@ export const savePost = createAsyncThunk("posts/savePost", async (postData) => {
   });
   export const getPosts = createAsyncThunk("post/getPosts", async () => {
     try {
-      const response = await axios.get("http://localhost:3001/getPosts");
+      const response = await axios.get(`https://myapp-server-qmdr.onrender.com/getPosts`);
       return response.data.posts;
       console.log(response);
     } catch (error) {
